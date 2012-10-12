4632e80ce5ae54999d801c3261daa6e5224b4d17
Per Andersen <post@perandersen.no>
2012-10-12 19:10:43 +0200

    EXPERIMENTAL - static me from fb image flying. No collision detection and images are rotated prior to spawning

--- beerprogcannon.js
+++ beerprogcannon.js
@@ -22,7 +22,7 @@
 
 //Function extending the array so we can remove objects
 
-Array.prototype.removeObject = function (element) 
+Array.prototype.remove = function (element) 
   {
           for (var i = 0; i < this.length; i++) 
        {
@@ -42,7 +42,7 @@ var cannonReady = true;
 //specifically the  point of the crossing between the cannon and the wheels...
 var CENTERPOINTX = 0;
 var CENTERPOINTY = 0;
-//just a varuiable used during diversion of degrees and radians
+//just a variable used during diversion of degrees and radians
 var degreeInRadians = 2 * Math.PI / 360;
 //how many balls have been fired?
 var ballsFired = 0;
@@ -53,8 +53,12 @@ var cannonBallStepper = setInterval("cannonBallStep()", 5);
 //we need to track the angle of the invisible line from the mouse x y
 //to the cannon centerpoint x y
 var angle = 0;
-var cannonOffset;			
+var cannonOffset;
 
+var peopleToGiveBeer = [];
+var TIMEBETWEENPERSONS = 600;
+var timeBeforeNextPerson = TIMEBETWEENPERSONS;
+var peopleAdded = 0;
 
 $(window).load(function() {
   init();
@@ -122,19 +126,28 @@ function cannonBallStep() {
     item.moveForward();
     if(isOutOfViewPort(item)) {
       item.destroy();
-      cannonBalls.removeObject(item);
+      cannonBalls.remove(item);
     }
   });
+  peopleToGiveBeer.forEach(function(item) {
+  	item.moveForward();
+  	    if(isOutOfViewPort(item)) {
+      item.destroy();
+      peopleToGiveBeer.remove(item);
+    }
+  });
+  
+  stepPeople();
 }
 
-function isOutOfViewPort(cannonBall) {
+function isOutOfViewPort(item) {
   var viewportHeight = $(window).height();
   var viewportWidth = $(window).width();
   
-  if(cannonBall.x < 0 
-     || cannonBall.y < 0
-     || cannonBall.x > viewportWidth 
-     || cannonBall.y > viewportHeight) {
+  if(item.x < 0 
+     || item.y < 0
+     || item.x > viewportWidth 
+     || item.y > viewportHeight) {
     return true;
   }
   else return false;
@@ -165,4 +178,75 @@ function CannonBall(angle, numberInRow) {
   this.destroy = function() {
     $("#ball" + this.number).remove();
   }
+}
+
+
+function Person(angle, numberInRow, fromWhere) {
+	
+  //fromWhere could be from the "top", "left" or "right"
+  this.fromWhere = fromWhere;
+  this.angle = angle;
+  this.number = numberInRow;
+  
+  this.top;
+  this.left;
+  //TODO Facebook integration
+  this.imgSrc = "staticPerson.jpg"
+  
+  if(fromWhere == "left") {
+  	this.top = 40;
+  	this.left = 0;
+  }
+  else if(fromWhere == "right") {
+  	this.top = 40;
+  	this.left = window.innerWidth;
+  }
+  
+  $("body").append("<div id='person" + this.number 
+                   + "' class='person' style='position:absolute; top:"
+		   + this.top 
+                   + "px;left:" + this.left 
+                   + "px;'><img src='"
+                   + this.imgSrc
+                   + "' id='personImg" 
+                   + this.number 
+                   + "'></div>");
+  $("#personImg" + this.number).rotate(angle);
+  this.element = $("#person" + this.number);
+
+  this.x = this.left;
+  this.y = this.top;
+
+  this.moveForward = function() {
+    this.x = this.x + Math.cos(degreeInRadians * this.angle);
+    this.y = this.y + Math.sin(degreeInRadians * this.angle);
+    this.element.css({top:this.y, left:this.x});	
+  }
+  
+  this.destroy = function() {
+    $("#person" + this.number).remove();
+  }
+}
+
+function stepPeople() {
+	if(timeBeforeNextPerson == 0) {
+		addNewPerson();
+		timeBeforeNextPerson = TIMEBETWEENPERSONS;
+	}
+	else timeBeforeNextPerson--;
+}
+
+function addNewPerson() {
+	peopleAdded++;
+	var rand = Math.floor((Math.random()*2)+1);
+	if(rand == 1) {
+		var person = new Person(180, peopleAdded, "right");
+		peopleToGiveBeer.push(person);		
+	} 
+	if(rand == 2) {
+		var person = new Person(0, peopleAdded, "left");
+		peopleToGiveBeer.push(person);	
+	}
+	
+	console.log(rand);
 }
